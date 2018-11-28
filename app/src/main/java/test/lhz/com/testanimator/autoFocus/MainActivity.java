3ee3629de3b5524e39ec58884f1294df913cea5b
LiuHanzhi <hanzhi@staff.sina.com>
2018-11-28 19:38:36 +0800

    焦点控制demo

---
+++ app/src/main/java/test/lhz/com/testanimator/autoFocus/MainActivity.java
@@ -0,0 +1,19 @@
+package test.lhz.com.testanimator.autoFocus;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+import test.lhz.com.testanimator.R;
+import test.lhz.com.testanimator.dispatchKeyEvent.FocusActivity;
+import test.lhz.com.testanimator.player.PlayerActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_autofocus);
+    }
+}
